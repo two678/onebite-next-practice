88a676f6e431544c4fdeeadc58ec795a4da4dd5e
two678 <two6788@gmail.com>
2025-05-23 19:48:36 +0900

    ✨ Feat: 데이터 패칭, 캐시 실습

--- src/app/(with-searchbar)/page.tsx
+++ src/app/(with-searchbar)/page.tsx
@@ -4,15 +4,14 @@ import { BookData } from "@/types";
 
 async function AllBooks() {
   const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`,
+    { cache: "no-store" }
   );
-
   if (!response.ok) {
-    return <div>오류가 발생했습니다...</div>;
+    return <div>오류가 발생했습니다 ...</div>;
   }
 
   const allBooks: BookData[] = await response.json();
-
   return (
     <div>
       {allBooks.map((book) => (
@@ -24,15 +23,14 @@ async function AllBooks() {
 
 async function RecoBooks() {
   const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/random`
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/random`,
+    { next: { revalidate: 3 } }
   );
-
   if (!response.ok) {
     return <div>오류가 발생했습니다...</div>;
   }
 
   const recoBooks: BookData[] = await response.json();
-
   return (
     <div>
       {recoBooks.map((book) => (
