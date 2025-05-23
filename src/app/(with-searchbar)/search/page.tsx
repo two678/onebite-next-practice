4116c03fcd4f5c789deb5b6c5885ba3aec16ea53
two678 <two6788@gmail.com>
2025-05-23 15:24:53 +0900

    ✨ Feat: 페이지 레이아웃  설정 실습

--- src/app/(with-searchbar)/search/page.tsx
+++ src/app/(with-searchbar)/search/page.tsx
@@ -4,5 +4,5 @@ export default async function Page({
   searchParams: Promise<{ q: string }>;
 }) {
   const { q } = await searchParams;
-  return <div>Search 페이지 : {q}</div>;
+  return <div>Search 페이지 {q}</div>;
 }
