407467ea60884a6c5d16f61a70f0c518cfd24440
two678 <two6788@gmail.com>
2025-05-24 18:04:35 +0900

    ✨ Feat: 컴포넌트 스트리밍 실습

--- src/app/(with-searchbar)/search/page.tsx
+++ src/app/(with-searchbar)/search/page.tsx
@@ -1,15 +1,10 @@
 import BookItem from "@/components/book-item";
 import { BookData } from "@/types";
 import { delay } from "@/util/delay";
+import { Suspense } from "react";
 
-export default async function Page({
-  searchParams,
-}: {
-  searchParams: Promise<{ q: string }>;
-}) {
+async function SearchResult({ q }: { q: string }) {
   await delay(1500);
-
-  const { q } = await searchParams;
 
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/search?q=${q}`,
@@ -28,5 +23,19 @@ export default async function Page({
         <BookItem key={book.id} {...book} />
       ))}
     </div>
+  );
+}
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: Promise<{ q: string }>;
+}) {
+  const { q } = await searchParams;
+
+  return (
+    <Suspense key={q || ""} fallback={<div>Loading ...</div>}>
+      <SearchResult q={q || ""} />
+    </Suspense>
   );
 }
