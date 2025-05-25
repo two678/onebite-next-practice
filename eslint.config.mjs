7d1be20ae91fda98f77318e975193acf156b6ee8
two678 <two6788@gmail.com>
2025-05-25 21:39:01 +0900

    chore: eslint 수정

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,11 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off", //any 허용
+    },
+  },
 ];
 
 export default eslintConfig;
