87d4898d1ad6af3576ab4d8d3c184da1669d886a
two678 <two6788@gmail.com>
2025-05-25 21:44:44 +0900

    Fix: 불필요한 코드 정리

--- src/app/(with-searchbar)/page.tsx
+++ src/app/(with-searchbar)/page.tsx
@@ -1,9 +1,6 @@
 import BookItem from "@/components/book-item";
 import style from "./page.module.css";
 import { BookData } from "@/types";
-import { delay } from "@/util/delay";
-import { Suspense } from "react";
-import BookListSkeleton from "@/components/skeleton/book-list-skeleton";
 import { Metadata } from "next";
 
 // export const dynamic = "force-dynamic";
@@ -14,7 +11,6 @@ import { Metadata } from "next";
 // 4. error : 페이지를 강제로 Static 페이지 설정 (설정하면 안되는 이유 -> 빌드 오류)
 
 async function AllBooks() {
-  await delay(1500);
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`,
     { cache: "force-cache" }
@@ -34,7 +30,6 @@ async function AllBooks() {
 }
 
 async function RecoBooks() {
-  await delay(3000);
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/random`,
     { next: { revalidate: 3 } }
@@ -53,8 +48,6 @@ async function RecoBooks() {
   );
 }
 
-export const dynamic = "force-dynamic";
-
 export const metadata: Metadata = {
   title: "한입 북스",
   description: "한입 북스에 등록된 도서를 만나보세요",
@@ -70,15 +63,11 @@ export default function Home() {
     <div className={style.container}>
       <section>
         <h3>지금 추천하는 도서</h3>
-        <Suspense fallback={<BookListSkeleton count={3} />}>
-          <RecoBooks />
-        </Suspense>
+        <RecoBooks />
       </section>
       <section>
         <h3>등록된 모든 도서</h3>
-        <Suspense fallback={<BookListSkeleton count={10} />}>
-          <AllBooks />
-        </Suspense>
+        <AllBooks />
       </section>
     </div>
   );
