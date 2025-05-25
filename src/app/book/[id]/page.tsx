5e0336806cabb69e12172537eccadd5685ef48ba
two678 <two6788@gmail.com>
2025-05-25 15:06:04 +0900

    ✨ Feat: 서버 액션 실습 - 1

--- src/app/book/[id]/page.tsx
+++ src/app/book/[id]/page.tsx
@@ -1,18 +1,12 @@
 import { notFound } from "next/navigation";
 import style from "./page.module.css";
 
-//export const dynamicParams = false;
 export function generateStaticParams() {
   return [{ id: "1" }, { id: "2" }, { id: "3" }];
 }
 
-export default async function Page({
-  params,
-}: {
-  params: Promise<{ id: string | string[] }>;
-}) {
+async function BookDetail({ params }: { params: Promise<{ id: string }> }) {
   const { id } = await params;
-
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/${id}`
   );
@@ -29,7 +23,7 @@ export default async function Page({
   const { title, subTitle, description, author, publisher, coverImgUrl } = book;
 
   return (
-    <div className={style.container}>
+    <section>
       <div
         className={style.cover_img_container}
         style={{ backgroundImage: `url('${coverImgUrl}')` }}
@@ -42,6 +36,34 @@ export default async function Page({
         {author} | {publisher}
       </div>
       <div className={style.description}>{description}</div>
+    </section>
+  );
+}
+
+function ReviewEditor() {
+  async function createReviewAction(formData: FormData) {
+    "use server";
+    const content = formData.get("content")?.toString();
+    const author = formData.get("author")?.toString();
+    console.log(content, author);
+  }
+
+  return (
+    <section>
+      <form action={createReviewAction}>
+        <input type="text" name="content" placeholder="리뷰 내용" />
+        <input type="text" name="author" placeholder="작성자" />
+        <button type="submit">작성하기</button>
+      </form>
+    </section>
+  );
+}
+
+export default function Page({ params }: { params: Promise<{ id: string }> }) {
+  return (
+    <div className={style.container}>
+      <BookDetail params={params} />
+      <ReviewEditor />
     </div>
   );
 }
